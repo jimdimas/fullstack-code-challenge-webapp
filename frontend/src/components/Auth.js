d1e8c875192ae6fe1e247c61ed49aeed14c79b8e
jimdimas <jimdimas6@gmail.com>
2024-09-23 17:11:19 +0300

    added css for register/login forms

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useAuth } from '../hooks/AuthProvider'
-
+import '../css/Auth.css'
 export default function Auth(props){
     const {isRegistered}=props
     const [credentials,setCredentials] = React.useState({
@@ -28,23 +28,23 @@ export default function Auth(props){
     }
 
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" value={credentials.username} onChange={handleChange}/>
-                <br/>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" value={credentials.password} onChange={handleChange}/>
-                <br/>
-                {!isRegistered && 
-                    <>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" value={credentials.email} onChange={handleChange}/>
-                        <br/>
-                    </>
-                }
-                <button>Submit</button>
-            </form>
+        <div class="access">
+            <div class="access-form">
+                <form class="form" onSubmit={onSubmit}>
+                    <fieldset class="username">
+                        <input type="text" placeholder="Username" id="username" name="username" value={credentials.username} onChange={handleChange}/>
+                    </fieldset>
+                    { props.isRegistered?'':
+                        <fieldset class="email">
+                            <input type="email" placeholder="Email" id="email" name="email" value={credentials.email} onChange={handleChange}/>
+                        </fieldset>
+                    }
+                    <fieldset class="password">
+                        <input type="password" placeholder="Password" id="password" name="password" value={credentials.password} onChange={handleChange}/>
+                    </fieldset>
+                    <button type="submit" class="btn">{props.isRegistered?"Login":"Register"}</button>
+                </form>
+            </div>
         </div>
     )
 }
