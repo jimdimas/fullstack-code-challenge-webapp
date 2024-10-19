c1777312813f1f02722d6f475cc701e9304c3a64
Dimitris Dimas <jimdimas6@gmail.com>
2024-10-19 04:05:38 +0300

    fixed bug in solve test where logout happened after assertion

--- backend/src/test/java/com/app/backend/tests/SolveTestTest.java
+++ backend/src/test/java/com/app/backend/tests/SolveTestTest.java
@@ -5,10 +5,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SolveTestTest extends BaseTest {
-
-    private TestPage testPage;
-    private ProfilePage profilePage;
-    private SolveTestPage solveTestPage;
     private TestResultPage testResultPage;
 
     @Test(priority = 1)
@@ -23,16 +19,13 @@ public class SolveTestTest extends BaseTest {
         solveTestPage.chooseAnswerToQuestion("What is 2*2?","2");
 
         testPage = solveTestPage.clickSubmitButton();
-        TestResultPage testResultPage = testPage.clickViewResults();
+        testResultPage = testPage.clickViewResults();
         Assert.assertEquals(testResultPage.checkTestResult("First Test"),"");
-        testResultPage.logoutAction();
     }
 
     @Test(priority = 2)
     public void solveTestPass() {
-        LoginPage loginPage = homePage.showLoginPage();
-        ProfilePage profilePage = loginPage.loginAction("user","123456");
-        TestPage testPage = profilePage.clickViewTests();
+        TestPage testPage = testResultPage.clickViewTests();
 
         SolveTestPage solveTestPage = testPage.clickTestByTitle("First Test");
         solveTestPage.chooseAnswerToQuestion("What is 1+1?","2");
@@ -42,13 +35,10 @@ public class SolveTestTest extends BaseTest {
         testPage = solveTestPage.clickSubmitButton();
         TestResultPage testResultPage = testPage.clickViewResults();
         Assert.assertEquals(testResultPage.checkTestResult("First Test"),"66%");
-        testResultPage.logoutAction();
     }
     @Test(priority = 3)
     public void solveTestImprove(){
-        LoginPage loginPage = homePage.showLoginPage();
-        ProfilePage profilePage = loginPage.loginAction("user","123456");
-        TestPage testPage = profilePage.clickViewTests();
+        TestPage testPage = testResultPage.clickViewTests();
 
         SolveTestPage solveTestPage = testPage.clickTestByTitle("First Test");
         solveTestPage.chooseAnswerToQuestion("What is 1+1?","2");
