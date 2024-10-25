6040ef4aa266bc5d1f17e275678c89480b2ab350
Dimitris Dimas <jimdimas6@gmail.com>
2024-10-25 19:20:04 +0300

    packages for mail functionality and mail bean setup

--- backend/src/main/java/com/app/backend/config/ApplicationConfig.java
+++ backend/src/main/java/com/app/backend/config/ApplicationConfig.java
@@ -2,8 +2,11 @@ package com.app.backend.config;
 
 import com.app.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,6 +15,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Properties;
 
 @Configuration
 @RequiredArgsConstructor
@@ -42,5 +47,27 @@ public class ApplicationConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender(
+            @Value("${spring.mail.host}") String host,
+            @Value("${spring.mail.port}") Integer port,
+            @Value("${spring.mail.protocol}") String protocol,
+            @Value("${spring.mail.username}") String username,
+            @Value("${spring.mail.password}") String password
+    ){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setProtocol(protocol);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.smtp.auth",true);
+        javaMailProperties.put("mail.smtp.starttls.enable",true);
+        javaMailProperties.put("mail.debug",true);
+        javaMailSender.setJavaMailProperties(javaMailProperties);
+        return javaMailSender;
     }
 }
