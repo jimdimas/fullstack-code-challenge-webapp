01e856b86c0898dd2d5f813480e136928e381da0
Dimitris Dimas <jimdimas6@gmail.com>
2024-10-25 23:19:45 +0300

    created security utils class with method for secure token creation

---
+++ backend/src/main/java/com/app/backend/util/SecurityUtils.java
@@ -0,0 +1,23 @@
+package com.app.backend.util;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class SecurityUtils {
+
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+    private static final String ALPHANUMERICS=UPPERCASE+LOWERCASE+NUMBERS;
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public static String createVerificationToken(){
+        StringBuilder verificationToken = new StringBuilder();
+        for (int i=0; i<32; i++){
+            verificationToken.append(ALPHANUMERICS.charAt(secureRandom.nextInt()));
+        }
+        return verificationToken.toString();
+    }
+}
